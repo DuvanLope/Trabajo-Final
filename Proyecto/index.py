3271d48576233459898f481d9136d0a3e671485d
Angie <vansa0917@gmail.comm>
2026-02-27 20:27:30 -0500

    pagina web de el dataset de aerolinea

---
+++ Proyecto/index.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+import streamlit as st
+
+url = "airlines.csv"
+
+airline = pd.read_csv(url)
+
+imagen = 
